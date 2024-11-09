3940b45586f16cc01f4eafb5748d911b541d2989
ThePaZiTiVik <pazitivik.ru@gmail.com>
2024-11-09 17:41:33 +0500

    Update message.py

--- message.py
+++ message.py
@@ -1,5 +1,7 @@
 KICK_MESSAGES = {
     "role_limit_exceeded": "Вы были кикнуты за слишком частую смену ролей.",
+    "webhook_limit_exceeded": "User kicked for exceeding webhook creation limit.",
+    "other_limit": "User kicked for exceeding other limits.",
     "channel_edit_limit_exceeded": "Вы были кикнуты за слишком частое изменение настроек каналов.",
     "channel_delete_limit_exceeded": "Вы были кикнуты за удаление каналов слишком много раз.",
     "admin_role_removed": "Вам была выдана роль с правами администратора, она была удалена.",
@@ -8,4 +10,5 @@ KICK_MESSAGES = {
     "channel_delete_limit_exceeded": "User kicked for exceeding the channel deletion limit.",
     "role_create_limit_exceeded": "User kicked for exceeding the role creation limit.",
     "channel_create_limit_exceeded": "User kicked for exceeding the channel creation limit."
+
 }
