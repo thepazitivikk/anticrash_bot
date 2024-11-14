3b02df00de1efe15225d6d72a1d6542bef697c20
ThePaZiTiVik <pazitivik.ru@gmail.com>
2024-11-14 19:52:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TOKEN = "ТОКЕН"
-WHITELIST = [1199916861236715540, 532909407345049601]
-ROLE_WHITELIST = [1077117671570669569]
+WHITELIST = []
+ROLE_WHITELIST = []
+ALLOWED_IDS = [] # тот кто может писать команды !catalog !setlimit !whitelist !rolelist
