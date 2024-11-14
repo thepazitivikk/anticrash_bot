da2ec4cc4d389880a4f250d29f4ae0938dad084d
ThePaZiTiVik <pazitivik.ru@gmail.com>
2024-11-14 19:51:57 +0500

    Update message.py

--- message.py
+++ message.py
@@ -7,9 +7,6 @@ KICK_MESSAGES = {
     "admin_role_removed": "Вам была выдана роль с правами администратора, она была удалена.",
     "bot_and_user_kicked": "Вы были кикнуты за добавление бота на сервер.",
     "webhook_deleted": "Ваш веб-хук был удален.",
-    "channel_delete_limit_exceeded": "User kicked for exceeding the channel deletion limit.",
-    "role_create_limit_exceeded": "User kicked for exceeding the role creation limit.",
-    "channel_create_limit_exceeded": "User kicked for exceeding the channel creation limit.",
-    "bot_add_limit_exceeded": "Превышен лимит участников, добавленных ботом."
-
+    "bot_add_limit_exceeded": "Exceeded bot add limit.",
 }
+
